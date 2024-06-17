fe33bbb1b3ea0a1f26e41a98bfc992e3038bfad7
FedericaSilver <federicaparillo.94@gmail.com>
2024-06-17 17:57:20 +0200

    Add files via upload

---
+++ Ereditarieta1/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Animale animale = new Animale();
+        animale.faIlVerso();
+        Gatto gatto = new Gatto();
+
+        gatto.faIlVerso();
+    }
+}
